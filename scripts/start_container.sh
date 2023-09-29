7d33a572d064cfd0dfd9fd4680a6ad8a5b53e9a3
Shubham Taware <93321554+shubzz-t@users.noreply.github.com>
2023-09-29 17:21:04 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,5 +5,5 @@ set -e
 docker pull shubzz/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 shubzz/simple-python-flask-app:latest
+docker run -d -p 5000:5000 shubzz/simple-python-flask-app:latest   
  
